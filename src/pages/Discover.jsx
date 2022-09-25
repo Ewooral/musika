aea964c0d97f0be95c8812bbd3f7af1f9dbe5454
Ewooral <boahen.cos@gmail.com>
2022-09-25 03:02:58 +0000

    feat: Create an identifier called genreTitle

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,13 +3,15 @@ import { genres } from '../assets/constants';
 
 const Discover = () => {
     // eslint-disable-next-line no-console
-    console.log(genres);
+    const genreTitle = 'Pop'
 
     return (
         <div className='flex flex-col'>
             <div className="w-full flex justify-between items-center
             sm:flex-row flex-col mt-4 mb-10">
-                <select />
+                <h2 className='font-bold text-3xl
+                text-white text-left'>Discover</h2>
+                <select name="" id=""></select>
             </div>
         </div>
     );
