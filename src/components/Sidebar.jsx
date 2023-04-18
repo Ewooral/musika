bcc40065d2eb6f1380dbf7c79c32d9b0f3d524a8
Ewooral <boahen.cos@gmail.com>
2023-04-18 23:54:57 +0000

    feat: Add sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { RiCloseLine } from "react-icons/ri";
 
 import { logo } from "../assets";
+import { links } from "../assets/constants";
 
 const Sidebar = () => <div>Sidebar</div>;
 
