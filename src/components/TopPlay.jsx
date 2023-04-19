c280f12632bda9b4b8c01b7a35d78e3b4bcf114b
Ewooral <boahen.cos@gmail.com>
2023-04-19 13:35:48 +0000

    feat: Create top play component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,12 @@
 import { useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { FreeMode } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import 'swiper/css'
+import 'swiper/css/free-mode'
+
 
 
 const TopPlay = () => {
