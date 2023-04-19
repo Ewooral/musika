c6ae8c935df5342a5b5838b8e893109531766289
Ewooral <boahen.cos@gmail.com>
2023-04-19 12:09:07 +0000

    feat: Add sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -39,8 +39,8 @@ const Sidebar = () => {
             <div className="absolute md:hidden block top-6
             right-3">
                 {mobileMenuOpen ? (
-                    <RiCloseLine />
-                ) : <HiOutlineMenu />
+                    <RiCloseLine className="w-6 h-6 text-white mr-2" />
+                ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" />
                 }
             </div>
 
