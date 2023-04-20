9d7ef1810c8c370914e1db2c528910448187ddba
Ewooral <boahen.cos@gmail.com>
2023-04-20 13:01:22 +0000

    feat: Create top play component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -41,8 +41,10 @@ const TopPlay = () => {
             <div className="w-full flex flex-col">
                 <div className="flex flex-row justify-between items-center">
                     <h2 className="text-white font-bold text-2xl">Top Charts</h2>
-                    <Link to="/top-chart"></Link>
-                    <p className="text-gray-300 text-base cursor-pointer">See more</p>
+                    <Link to="/top-chart">
+                        <p className="text-gray-300 text-base cursor-pointer">See more</p>
+                    </Link>
+
                 </div>
             </div>
 
