f343dbd256011425606bd91a8d9c633ffb30aafc
Ewooral <boahen.cos@gmail.com>
2023-04-20 14:51:22 +0000

    feat: Create top play component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -86,7 +86,13 @@ const TopPlay = () => {
                             key={song?.key}
                             style={{ width: '25%', height: 'auto' }}
                             className="shadow-lg rounded-full animate-slideright"
-                        />
+                        >
+                            <Link to={`/artists/${song?.artists[0].adamid}`}>
+                                <img src={song?.images.background} alt="name"
+                                    className="rounded-full w-full object-cover" />
+                            </Link>
+
+                        </SwiperSlide>
                     ))}
 
                 </Swiper>
