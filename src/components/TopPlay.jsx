cf19600e3f3c13162ea1e1c6223eb4f7228f4edb
Ewooral <boahen.cos@gmail.com>
2023-04-20 15:00:39 +0000

    feat: Create top play component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -15,7 +15,10 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 const TopChartCard = ({ song, i }) => (
     <div className="w-full flex flex-row items-center
      hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-        {song.title}
+        <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+        <div className="flex-1 flex flex-row justify-between items-center">
+            <img src={song?.images?.coverart} alt={song?.title} />
+        </div>
     </div>
 )
 const TopPlay = () => {
