e22a800cbf0209d576fc3c38499d0e42ea56e6d7
Ewooral <boahen.cos@gmail.com>
2023-04-24 23:20:44 +0000

    Add DetailsHeader component

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -17,7 +17,7 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
                     />
 
                     <div className="ml-5">
-                        <p>{artistId ? artist?.name : songData?.title}</p>
+                        <p className="font-bold sm:text-3xl text-xl text-white">{artistId ? artist?.name : songData?.title}</p>
                     </div>
 
                 </div>
